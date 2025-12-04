93154ee27608b4d901500bfa8aa2a318ac5ec6db
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-12-04 11:13:32 +0530

    Initial commit

---
+++ A04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace A04;
+
+class Program {
+   static void Main () {
+      var allWords = File.ReadAllLines ("words.txt").Select (w => w.Trim ().ToLower ()).Where (w => w.Length >= 4);
+      var countDict = allWords.SelectMany (w => w).GroupBy (c => c).ToDictionary (g => g.Key, g => g.Count ());
+      var mostCommonChars = countDict.OrderByDescending (kvp => kvp.Value).Take (7).Select (kvp => (kvp.Key, kvp.Value)).ToList ();
+      foreach (var (chr, cnt) in mostCommonChars) Console.WriteLine ($"{chr}-{cnt}");
+   }
+}
