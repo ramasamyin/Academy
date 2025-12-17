24ff799423039e2cf44c1b7fbca0532025362ab1
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-12-17 11:39:39 +0530

    Optimizing the code

--- A04/Program.cs
+++ A04/Program.cs
@@ -9,9 +9,11 @@ namespace A04;
 
 class Program {
    static void Main () {
-      var allWords = File.ReadAllLines ("words.txt").Select (w => w.Trim ().ToLower ()).Where (w => w.Length >= 4);
-      var countDict = allWords.SelectMany (w => w).GroupBy (c => c).ToDictionary (g => g.Key, g => g.Count ());
-      var mostCommonChars = countDict.OrderByDescending (kvp => kvp.Value).Take (7).Select (kvp => (kvp.Key, kvp.Value)).ToList ();
-      foreach (var (chr, cnt) in mostCommonChars) Console.WriteLine ($"{chr}-{cnt}");
+      var allWords = File.ReadAllLines ("words.txt");
+      var mostCommonChars = allWords.SelectMany (w => w)
+                                    .GroupBy (c => c)
+                                    .OrderByDescending (g => g.Count ())
+                                    .Take (7);
+      foreach (var g in mostCommonChars) Console.WriteLine ($"{g.Key}-{g.Count ()}");
    }
 }
