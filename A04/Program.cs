63a82bf2b3d6263705ad3b7f5b141dc1e736dde5
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-12-23 15:14:17 +0530

    Simplified GetFreq2

--- A04/Program.cs
+++ A04/Program.cs
@@ -5,36 +5,35 @@
 // Program.cs
 // To build a frequency table with occurrence of all the letters
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
 namespace A04;
 
 class Program {
    static void Main () {
       var words = File.ReadAllText ("words.txt");
-      // Using LINQ
-      var freq1 = GetFreq1 (words);
-      Console.WriteLine ("Using LINQ");
-      foreach (var (Letter, Count) in freq1) Console.WriteLine ($"{Letter} - {Count}");
-      // Without using LINQ
-      var freq2 = GetFreq2 (words);
-      Console.WriteLine ("\nWithout using LINQ");
-      foreach (var (Key, Value) in freq2) Console.WriteLine ($"{Key} - {Value}");
+      PrintFrequency ("Using LINQ", GetFreq1 (words));
+      PrintFrequency ("\nWithout using LINQ", GetFreq2 (words));
    }
 
-   static IEnumerable<(char Letter, int Count)> GetFreq1 (string words) => words.Where (char.IsLetter)
+   // Using LINQ
+   static IEnumerable<KeyValuePair<char, int>> GetFreq1 (string words) => words.Where (char.IsLetter)
                                                                                 .GroupBy (c => c)
-                                                                                .Select (g => (Letter: g.Key, Count: g.Count ()))
-                                                                                .OrderByDescending (g => g.Count)
+                                                                                .Select (g => new KeyValuePair<char, int> (g.Key, g.Count ()))
+                                                                                .OrderByDescending (g => g.Value)
                                                                                 .Take (7);
 
+   // Without using LINQ
    static IEnumerable<KeyValuePair<char, int>> GetFreq2 (string words) {
       Dictionary<char, int> charFrequency = [];
-      for (char c = 'A'; c <= 'Z'; c++) {
-         int count = 0;
-         foreach (char ch in words) {
-            if (ch == c) count++;
-         }
-         charFrequency[c] = count;
+      for (char c = 'A'; c <= 'Z'; c++) charFrequency[c] = 0;
+      foreach (char ch in words) {
+         if (ch >= 'A' && ch <= 'Z') charFrequency[ch]++;
       }
       return charFrequency.OrderByDescending (c => c.Value).Take (7);
+   }
+
+   static void PrintFrequency (string title, IEnumerable<KeyValuePair<char, int>> freq) {
+      WriteLine (title);
+      foreach (var (key, value) in freq) WriteLine ($"{key} - {value}");
    }
 }
