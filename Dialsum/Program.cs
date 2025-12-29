cae54094e5f929b423674ea0cd22fb0480579f5e
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-12-29 13:14:40 +0530

    Initial commit

---
+++ Dialsum/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] instructions = File.ReadAllLines ("C:\\Work\\Academy\\Dialsum\\dial.txt");
+int start = 50;
+int cnt = 0;
+int currentPosition;
+foreach (string line in instructions) {
+   string[] val = line.Split (line[0]);
+   int value = int.Parse (val[1]);
+   if (line[0] == 'R') {
+      currentPosition = (start + value) % 100;
+      if (currentPosition == 0) cnt++;
+      start = currentPosition;
+   } else {
+      int sub = start - value;
+      currentPosition = sub < 0 ? sub + 100 : sub;
+      if (currentPosition == 0) cnt++;
+      start = currentPosition;
+   }
+}
+Console.WriteLine (cnt);
