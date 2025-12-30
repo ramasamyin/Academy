3a5df81ecc97dfa22c0688ebed6afba5603f5d3c
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-12-30 16:43:42 +0530

    Keep current version, discard stash changes

---
+++ Dialsum/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] instructions = File.ReadAllLines ("dial.txt");
+int start = 50;
+int cnt = 0;
+int currentPosition;
+foreach (string line in instructions) {
+   string[] val = line.Split (line[0]);
+   int value = int.Parse (val[1]);
+   if (line[0] == 'R') {
+      currentPosition = (start + value) % 100;
+      if (currentPosition == 0) cnt++;
+      start = currentPosition;
+   } else {
+      int sub = start - value;
+      currentPosition = sub < 0 ? (sub + 100)%100 : sub%100;
+      if (currentPosition == 0) cnt++;
+      start = currentPosition;
+   }
+}
+Console.WriteLine (cnt);
